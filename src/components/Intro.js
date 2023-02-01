a81fdafa5a5c7a0e7dba1d51ffb945cb6124765b
Diego Bueno <diegobuenomendonca@hotmail.com>
2023-02-01 10:37:03 -0300

    feat: adiciona pequena intro

---
+++ src/components/Intro.js
@@ -0,0 +1,10 @@
+function Intro() {
+  return (
+    <div>
+      <h1>Olá! Me chamo Diego Mendonça e sou Dev Full-stack!</h1>
+      <h2>Bem-vindo(a) e boa leitura!</h2>
+    </div>
+  );
+}
+
+export default Intro
