ff57e6aaae6ce41165575ac99fbd38ce01655dab
Diego Bueno <diegobuenomendonca@hotmail.com>
2023-02-17 14:39:59 -0300

    style: contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,26 +4,28 @@ function Contact() {
   return (
     <section id="contacts">
       <h2>Contatos</h2>
-      <p>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/diego-bueno-mendonca-dev-full-stack/"
-        >
-          Linkedin 
-        </a>
-      </p>
-      <p>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://github.com/Diego-Bueno-M"
-        >
-          GitHub 
-        </a>
-      </p>
-      <p>Email: diegobuenomendonca@hotmail.com</p>
-      <p>WhatsApp: (11) 95320-5179</p>
+      <div>
+        <p>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/diego-bueno-mendonca-dev-full-stack/"
+          >
+            Linkedin 
+          </a>
+        </p>
+        <p>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Diego-Bueno-M"
+          >
+            GitHub
+          </a>
+        </p>
+        <p>Email: diegobuenomendonca@hotmail.com</p>
+        <p>WhatsApp: (11) 95320-5179</p>
+      </div>
     </section>
   );
 }
