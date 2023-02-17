1cfc259b8e1c8dd20cfbfdd586f59257ba667ee6
Diego Bueno <diegobuenomendonca@hotmail.com>
2023-02-17 10:52:37 -0300

    style: estilo inicial para about

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,31 @@
 import '../styles/About.css'
+import profile_photo from '../images/profilePhoto.png'
 
 function About() {
   return (
     <section id="about">
-      <img src="../images/foto_perfil.png" alt="profile_photo"/>
-      <h2>Um pouco sobre mim e minha trajetória</h2>
-      <p>
-        Tenho 23 anos, moro em São Bernardo do Campo e sempre tive
-        afinadade e paixão por tecnologia.<br></br>
-        Desde de pequeno eu era o "técnico de TI" da família, sempre
-        pesquisava sobre como resolver os problemas com a impressora,
-        wi-fi, etc.<br></br>
-        Iniciei meus estudos na área de desenvolvimento web em Janeiro
-        de 2022 na Trybe, estou na reta final dos meus estudos finalizando-os
-        em Fevereiro de 2023.<br></br>Já tenho bagagem e experiência para atuar
-        como desenvolvedor Full-Stack Júnior, devivo aos inúmeros projetos
-        realizados durante meus estudos.<br></br>
-        Atualmente meu principal objetivo é encontrar minha primeira
-        oportunidade na área de desenvolvimento de software
-        e colocar minhas habilidades em prática.<br></br>Busco uma empresa que
-        me permita crescer, inovar e aprender todos os dias. Quero sair da zona
-        de conforto e sei que ainda tenho um longo caminho pela frente.<br></br>
-        Um dia serei referência na área de desenvolvimento de software.
-      </p>
+      <img src={profile_photo} alt="profile_photo"/>
+      <div>
+        <h2>Um pouco sobre mim e minha trajetória</h2>
+        <p>
+          Tenho 23 anos, moro em São Bernardo do Campo e sempre tive
+          afinadade e paixão por tecnologia.<br></br>
+          Desde de pequeno eu era o "técnico de TI" da família, sempre
+          pesquisava sobre como resolver os problemas com a impressora,
+          wi-fi, etc.<br></br>
+          Iniciei meus estudos na área de desenvolvimento web em Janeiro
+          de 2022 na Trybe e finalizei em Fevereiro em Fevereiro de
+          2023.<br></br>Já tenho bagagem e experiência para atuar
+          como desenvolvedor Full-Stack Júnior, devivo aos inúmeros projetos
+          realizados durante meus estudos.<br></br>
+          Atualmente meu principal objetivo é encontrar minha primeira
+          oportunidade na área de desenvolvimento de software
+          e colocar minhas habilidades em prática.<br></br>Busco uma empresa que
+          me permita crescer, inovar e aprender todos os dias. Quero sair da zona
+          de conforto e sei que ainda tenho um longo caminho pela frente.<br></br>
+          Um dia serei referência na área de desenvolvimento de software.
+        </p>
+      </div>
     </section>
   );
 }
