9025e592bc23fd62049c0646a6fd48eec874ac1d
Diego Bueno <diegobuenomendonca@hotmail.com>
2023-02-17 16:30:31 -0300

    feat: adiciona um projectCard de teste

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,12 +1,17 @@
-// import ProjectCard from "../components/ProjectCard";
+import ProjectCard from "../components/ProjectCard";
 import '../styles/Projects.css'
+import intro from '../images/intro.jpg'
 
 function Project() {
   return (
     <section id="projects">
       <h2>Principais Projetos</h2>
       <div>
-        Em Breve
+        <ProjectCard
+          name="Projeto Teste"
+          image={intro}
+          stacks={['stack1', 'stack2', 'stack3']}
+        />
       </div>
     </section>
   );
