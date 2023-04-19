dfb70072e8a585b3c4a29b0a5d31890fec070916
Diego Bueno <diegobuenomendonca@hotmail.com>
2023-04-19 15:51:46 -0300

    styles: pequenos ajustes em Projects e projectCard

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,24 +5,26 @@ import techNwes from '../images/techNwes.png'
 
 function Project() {
   return (
-    <section id="projects">
-      <h2>Principais Projetos</h2>
-      <div>
-        <ProjectCard
-          name="Projeto Sistema Solar"
-          image={solarSystem}
-          stacks={['React', 'JSX']}
-          url='https://github.com/Diego-Bueno-M/Project-Solar-System'
-        />
+    <section id="projects-section">
+      <h2 id="projects-h2">Principais Projetos</h2>
+      <div id ="projects-div">
+        <div id="projects-card">
+          <ProjectCard
+            name="Projeto Sistema Solar"
+            image={solarSystem}
+            stacks={['React', 'JSX']}
+            url='https://github.com/Diego-Bueno-M/Project-Solar-System'
+          />
+        </div>
+        <div id="projects-card">
+          <ProjectCard
+            name="Tech News"
+            image={techNwes}
+            stacks={['Pyhton', 'BeautifulSoup', 'parsel', 'pymongo']}
+            url='https://github.com/Diego-Bueno-M/tech-News'
+          />
+        </div>
       </div>
-      <div>
-        <ProjectCard
-          name="Tech News"
-          image={techNwes}
-          stacks={['Pyhton', 'BeautifulSoup', 'parsel', 'pymongo']}
-          url='https://github.com/Diego-Bueno-M/tech-News'
-        />
-      </div>     
     </section>
   );
 }
