99f6eda5bce091705409e94d500dfc7b15d8ddcd
Diego Bueno <diegobuenomendonca@hotmail.com>
2023-04-19 15:31:01 -0300

    feat: adiciona card do projeto tech news e imagem

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,7 @@
 import ProjectCard from "../components/ProjectCard";
 import '../styles/Projects.css'
 import solarSystem from '../images/solarSystem.png'
+import techNwes from '../images/techNwes.png'
 
 function Project() {
   return (
@@ -12,6 +13,24 @@ function Project() {
           image={solarSystem}
           stacks={['React', 'JSX']}
           url='https://github.com/Diego-Bueno-M/Project-Solar-System'
+        />
+      </div>
+      <h2>Principais Projetos</h2>
+      <div>
+        <ProjectCard
+          name="Projeto Sistema Solar"
+          image={solarSystem}
+          stacks={['Pyhton', 'BeautifulSoup', 'parsel', 'pymongo']}
+          url='https://github.com/Diego-Bueno-M/Project-Solar-System'
+        />
+      </div>     
+      <h2>Tech News</h2>
+      <div>
+        <ProjectCard
+          name="Tech News"
+          image={techNwes}
+          stacks={['React', 'JSX']}
+          url='https://github.com/Diego-Bueno-M/tech-News'
         />
       </div>
     </section>
