a3b81625e961390d6039859e26fdaf80b29219be
Diego Bueno <diegobuenomendonca@hotmail.com>
2024-08-09 19:39:56 -0300

    altera o css dos cards de projetos e adiciona a janelinha modal

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,31 +1,42 @@
-import '../styles/ProjectCard.css'
+import React, { useState } from 'react';
+import '../styles/ProjectCard.css';
+import Modal from './Modal';
 
 function ProjectCard(props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleImageClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="project-card">
-      <h3>
-        { props.name }
-      </h3>
+      <h3>{props.name}</h3>
       <img
         alt="project thumb"
         src={props.image}
+        onClick={handleImageClick}
+        className="clickable-image"
       />
-      <ul>
-        <h4>Tecnologias utilizadas:</h4>
-        { props.stacks.map((stack, index) => {
-          return( 
-            <li key={`${ stack }-${ index }`}>
-              {stack}
-            </li>
-          );
-        }) }
-      </ul>
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href={ props.url }>
-        <button type="button">Repositório</button>
-      </a>
+      {isModalOpen && (
+        <Modal onClose={handleCloseModal}>
+          <div className="modal-content">
+            <h4>Tecnologias utilizadas:</h4>
+            <ul>
+              {props.stacks.map((stack, index) => (
+                <li key={`${stack}-${index}`}>{stack}</li>
+              ))}
+            </ul>
+            <a target="_blank" rel="noreferrer" href={props.url}>
+              Ver projeto
+            </a>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 }
