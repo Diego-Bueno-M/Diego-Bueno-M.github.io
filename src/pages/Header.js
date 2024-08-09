3573b268aea58eb241ce459640988c8b4742bbb0
Diego Bueno <diegobuenomendonca@hotmail.com>
2024-08-09 19:24:33 -0300

    adiciona carrossel e pequeno ajuste no header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -6,7 +6,7 @@ function Header() {
       <nav>
         <a href="#intro" className="nav-link">Intro</a>
         <a href="#about" className="nav-link">Sobre Mim</a>
-        <a href="#projects" className="nav-link">Projetos</a>
+        <a href="#projects-section" className="nav-link">Projetos</a>
         <a href="#contacts" className="nav-link">Contato</a>
       </nav>
     </header>
--- src/pages/Techs.js
+++ src/pages/Techs.js
@@ -1,20 +1,48 @@
+import React, { useState } from 'react';
+import { useSpring, animated } from '@react-spring/web';
 import '../styles/Techs.css';
 
 function Techs() {
-  const javascriptImg = 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg';
-  const reactImg = 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg';
-  const nodeImg = 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg';
-  const pythonImg = 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg';
-  const mysqlImg = 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg';
+  const images = [
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg', alt: 'JavaScript' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg', alt: 'React' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg', alt: 'Node.js' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg', alt: 'Python' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg', alt: 'MySQL' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg', alt: 'Java' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg', alt: 'C#' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ruby/ruby-original-wordmark.svg', alt: 'Ruby' },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const { transform } = useSpring({
+    transform: `rotateY(${currentIndex * -45}deg)`,
+    config: { tension: 300, friction: 30 },
+  });
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => prevIndex - 1);
+  };
 
   return (
-    
     <section id="techs">
-      <img src={javascriptImg} alt="javaScript icon" />
-      <img src={reactImg} alt="react icon"/>
-      <img src={nodeImg} alt="node icon"/>
-      <img src={pythonImg} alt="python icon" />
-      <img src={mysqlImg} alt="mysql icon"/>
+      <h2>Technologies</h2>
+      <div className="carousel">
+        <animated.div className="carousel-inner" style={{ transform }}>
+          {images.map((image, index) => (
+            <div className="carousel-item" key={index} style={{ transform: `rotateY(${index * 45}deg) translateZ(300px)` }}>
+              <img src={image.src} alt={image.alt} draggable="false" />
+            </div>
+          ))}
+        </animated.div>
+        <div className="arrow left" onClick={handlePrev}>&#9664;</div>
+        <div className="arrow right" onClick={handleNext}>&#9654;</div>
+      </div>
     </section>
   );
 }
